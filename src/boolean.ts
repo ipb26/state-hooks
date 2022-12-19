63d35e1e2eb10b3289d682c5490b31af60d4dab1
Sam Barrow <sam@sambarrow.com>
2022-12-19 16:05:36 -0500

    .

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,12 +1,19 @@
 import { useCallback, useState } from "react"
 
 /**
- * A boolean hook that provides convenience methods.
+ * 
  */
-export const useBoolean = (initialValue: boolean) => {
+type BooleanState = { value: boolean, on(): void, off(): void, toggle(): void, set(value: boolean): void }
+
+/**
+ * Returns a boolean state object.
+ * @param initialValue An initial boolean value.
+ * @returns A boolean state object.
+ */
+export function useBoolean(initialValue: boolean): BooleanState {
     const [value, set] = useState(initialValue)
     const on = useCallback(() => set(true), [])
     const off = useCallback(() => set(false), [])
     const toggle = useCallback(() => set(state => !state), [])
-    return [value, on, off, toggle, set] as const
+    return { value, on, off, toggle, set } as const
 }
