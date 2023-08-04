3825866bc2c48789bb8dade789e1e48e8b7710ef
Sam Barrow <sam@sambarrow.com>
2023-08-04 12:26:09 -0400

    .

--- src/async.ts
+++ src/async.ts
@@ -1,34 +1,6 @@
 import { useEffect, useState } from "react"
 import { useBoolean } from "./boolean"
 
-type AsyncCallback<R, A extends readonly unknown[]> = (...args: A) => Promise<R>
-
-export function useMutex() {
-    const [promise, setPromise] = useState<Promise<void>>()
-    const locked = useBoolean()
-    const lock = async () => {
-        if (promise !== undefined) {
-            await promise
-        }
-        locked.on()
-    }
-    return {
-        isLocked: locked.value,
-        lock: locked.on,
-        release: locked.off,
-        runExclusive: async <T>(func: () => Promise<T>) => {
-            locked.on()
-            try {
-
-            }
-            catch (e) {
-                //TODO throw err
-                locked.off()
-            }
-        }
-    }
-}
-
 export function useAsyncCallback<R, A extends readonly unknown[]>(func: (...args: A) => Promise<R>) {
     const busy = useBoolean()
     const [result, setResult] = useState<PromiseSettledResult<unknown>>()
@@ -63,31 +35,4 @@ export function useAsyncCallback<R, A extends readonly unknown[]>(func: (...args
             }
         }
     }
-    /*
-        const [promise, setPromise] = useState<Promise<R>>()
-    
-    
-    
-        const [pending, setPending] = useState(0)
-        useEffect(() => {
-            if (promise !== undefined) {
-                setPending(_ => _ + 1)
-                promise.then(value => setResult({ status: "fulfilled", value }), reason => setResult({ status: "rejected", reason }))
-            }
-        }, [
-            promise
-        ])
-        useEffect(() => {
-            if (result !== undefined) {
-                if (result.status === "rejected") {
-                    throw result.reason
-                }
-            }
-        }, [
-            result
-        ])
-        const run = useCallback(async (...args: A) => setPromise(options.callback(...args)), [options.callback])
-        return {
-    
-        }*/
 }
