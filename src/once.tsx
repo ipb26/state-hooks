d5cb4c945a851b80a334b78eaa031ce51756165f
Sam Barrow <sam@sambarrow.com>
2023-09-29 08:54:14 -0400

    .

--- src/once.tsx
+++ src/once.tsx
@@ -1,7 +1,10 @@
-import { useCallback, useEffect, useMemo } from "react"
+import { EffectCallback, useCallback, useEffect, useMemo } from "react"
 
 export function useCallbackOnce<T extends Function>(callback: T) {
     return useCallback(callback, [])
+}
+export function useEffectOnce(effect: EffectCallback) {
+    return useEffect(effect, [])
 }
 export function useMemoOnce<T>(factory: () => T) {
     return useMemo(factory, [])
