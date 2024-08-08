1dc1ff909a045962e366cc0b19c08d6a72763648
Sam Barrow <sam@sambarrow.com>
2024-08-08 15:04:32 +0000

    .

--- src/time.ts
+++ src/time.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useBoolean } from "./boolean";
 import { useUpdateEffect } from "./updates";
 
@@ -23,4 +23,33 @@ export function useAt(time: number) {
         at
     ])
     return passed.value
+}
+
+export function useOptionalAt(time: number | undefined) {
+    const until = time === undefined ? undefined : time - Date.now()
+    const [passed, setPassed] = useState<boolean | undefined>(until === undefined ? undefined : until <= 0)
+    useUpdateEffect(() => {
+        if (until === undefined) {
+            return
+        }
+        setPassed(until <= 0)
+    }, [
+        until
+    ])
+    const at = until === undefined ? undefined : until <= 2147483647 ? until : 0
+    useEffect(() => {
+        if (at === undefined) {
+            return
+        }
+        if (at <= 0) {
+            return
+        }
+        const timeout = setTimeout(() => setPassed(true), at)
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [
+        at
+    ])
+    return passed
 }
