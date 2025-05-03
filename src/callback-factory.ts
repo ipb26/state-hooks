ccde8394e59ee6b361cb861db9be30699ace83c4
Sam Barrow <sam@sambarrow.com>
2025-05-03 22:08:43 +0000

    .

---
+++ src/callback-factory.ts
@@ -0,0 +1,15 @@
+import { DependencyList, useCallback } from "react"
+import { useCustomCompareCallback } from "./custom-compare"
+import { useDeepCompareCallback } from "./deep-compare"
+import { DepsAreEqual } from "./types"
+
+
+export function useCallbackFactory<T, D extends DependencyList>(factory: (...args: D) => T, deps: [...D]) {
+    return useCallback(() => factory(...deps), deps)
+}
+export function useCustomCompareCallbackFactory<T, D extends DependencyList>(factory: (...args: D) => T, deps: [...D], depsAreEqual: DepsAreEqual<readonly [...D]>) {
+    return useCustomCompareCallback(() => factory(...deps), deps, depsAreEqual)
+}
+export function useDeepCompareCallbackFactory<T, D extends DependencyList>(factory: (...args: D) => T, deps: [...D]) {
+    return useDeepCompareCallback(() => factory(...deps), deps)
+}
